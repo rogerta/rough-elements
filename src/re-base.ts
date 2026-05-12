820edb12e562bb2d4a432ba3bc37640f9ed30859
Roger Tawa <roger@tawacentral.net>
2026-05-12 15:07:13 -0400

    Got basic card working with styles

--- src/re-base.ts
+++ src/re-base.ts
@@ -31,46 +31,58 @@ export class ReBase extends LitElement {
     }
 
     this.observer_ = new ResizeObserver(entries => {
-      if (!this.svg_) {
+      if (!this.svg_ || !this.rough_) {
         throw new Error('Failed to find svg element')
       }
 
       for (let entry of entries) {
         const { width, height } = this.svg_.getBoundingClientRect()
-        const bwidth =
-            parseFloat(getComputedStyle(entry.target).borderWidth)
-        let rect: SVGElement | undefined = undefined
+        const cstyles = getComputedStyle(entry.target)
+        const bwidth = parseFloat(cstyles.borderWidth)
+        const halfbwidth = bwidth / 2
+        const bcolour = cstyles.getPropertyValue('--re-fill')
+        const roughElements: SVGElement[] = []
+
+        if (bcolour) {
+          const options = Object.assign({
+            maxRandomnessOffset: halfbwidth,
+            fill: bcolour,
+            fillStyle: 'hachure',
+            fillWeight: 16,  // Should match --re-fill-width below.
+            hachureGap: 17,
+            hachureAngle: -60,
+          }, this.options_)
+          roughElements.push(this.rough_.rectangle(
+              -halfbwidth, -halfbwidth,
+              width + bwidth, height + bwidth,
+              options))
+        }
+
         if (bwidth > 0) {
-          const halfbwidth = bwidth / 2
           const options = Object.assign({
             maxRandomnessOffset: halfbwidth,
           }, this.options_)
-          rect = this.rough_?.rectangle(
+          roughElements.push(this.rough_.rectangle(
               -halfbwidth, -halfbwidth,
               width + bwidth, height + bwidth,
-              options)
+              options))
         }
 
-        if (rect) {
-          this.svg_.replaceChildren(rect)
-        } else {
-          this.svg_.replaceChildren()
-        }
+        this.svg_.replaceChildren(...roughElements)
       }
     })
     this.observer_.observe(this, {box: 'border-box'})
   }
 
-  override render() {
-    return [html`<svg></svg>`]
+  renderRoughSvg() {
+    return [html`<svg id="rough"></svg>`]
   }
 
   static styles = [
     css`
       :host {
         /* Make sure the browser will not draw any visible border even when
-        * the border width is other than zero.
-        */
+        * the border width is other than zero. */
         border-color: transparent;
         border-style: solid;
         border-width: 0;
@@ -79,22 +91,62 @@ export class ReBase extends LitElement {
         position: relative;
 
         /* It's important that the element box sizing be border-box so that the
-        * rough border is re-drawn when the element's border width changes.
-        * Otherwise the resize observer would not get called. */
+         * rough border is re-drawn when the element's border width changes.
+         * Otherwise the resize observer would not get called. */
         box-sizing: border-box;
+
+        /* Should match fillWeight above */
+        --re-fill-width: 16;
       }
 
       /* The svg element is positioned to covers the padding box of the host
-      * element. Overflow is visible so the rough border (SVG sub-elements) will
-      * can placed in the host's border box, which is outside the padding box.
-      */
-      svg {
+       * element. Overflow is visible so the rough border (SVG sub-elements)
+       * will can placed in the host's border box, which is outside the
+       * padding box. */
+      #rough {
         position: absolute;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
         overflow: visible;
+      }
+
+      /* Styling for the outline of shapes.  Anything that affects the stroke
+       * of an SVG element can be used here.  Fill is always 'none'. */
+      #rough .outline {
+        fill: none;
+        stroke: var(--re-stroke, currentcolor);
+        stroke-dasharray: var(--re-stroke-dasharray, inherit);
+        stroke-dashoffset: var(--re-stroke-dashoffset, inherit);
+        stroke-linecap: var(--re-stroke-linecap, inherit);
+        stroke-linejoin: var(--re-stroke-linejoin, inherit);
+        stroke-miterlimit: var(--re-stroke-miterlimit, inherit);
+        stroke-opacity: var(--re-stroke-opacity, inherit);
+        stroke-width: var(--re-stroke-width, inherit);
+      }
+
+      /* Styling for the solid fill of shapes.  Anything that affects the fill
+       * of an SVG element can be used here.  Stroke is always 'none'. */
+      #rough .solid-fill {
+        stroke: none;
+        fill: var(--re-fill, inherit);
+        fill-opacity: var(--re-fill-opacity, inherit);
+        fill-rule: var(--re-fill-rule, inherit);
+      }
+
+      /* Styling for the pattern fill of shapes.  Anything that affects the
+       * stroke of an SVG element can be used here.  Fill is always 'none'. */
+      #rough .pattern-fill {
+        fill: none;
+        stroke: var(--re-fill, inherit);
+        stroke-dasharray: var(--re-fill-dasharray, inherit);
+        stroke-dashoffset: var(--re-fill-dashoffset, inherit);
+        stroke-linecap: var(--re-fill-linecap, inherit);
+        stroke-linejoin: var(--re-fill-linejoin, inherit);
+        stroke-miterlimit: var(--re-fill-miterlimit, inherit);
+        stroke-opacity: var(--re-fill-opacity, inherit);
+        stroke-width: var(--re-fill-width, inherit);
       }
     `]
 }
--- src/re-card.ts
+++ src/re-card.ts
@@ -1,22 +1,62 @@
-import { css, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { css, html, type PropertyValues } from 'lit'
+import { customElement, query } from 'lit/decorators.js'
 
 import { ReBase } from './re-base.js'
 
 @customElement('re-card')
 export class Element extends ReBase {
+  @query('slot[part=header') slotHeader!: HTMLSlotElement
+  @query('slot[part=body') slotBody!: HTMLSlotElement
+  @query('slot[part=footer') slotFooter!: HTMLSlotElement
+  @query('slot[part=image') slotImage!: HTMLSlotElement
+
   override render() {
-    return super.render().concat([
-      html`<slot></slot>`
-    ])
+    return [
+      super.renderRoughSvg(),
+      html`
+        <slot class="hidden" part="image" name="image"></slot>
+        <slot class="hidden" part="header" name="header"></slot>
+        <slot class="hidden" part="body"></slot>
+        <slot class="hidden" part="footer" name="footer"></slot>
+      `,
+    ]
+  }
+
+  override updated(_props: PropertyValues) {
+    ['header', 'body', 'footer', 'image'].forEach(part => {
+      const slot =
+          this.renderRoot.querySelector<HTMLSlotElement>(`slot[part=${part}]`)!
+      const hasChildren = slot.assignedNodes().length > 0
+      slot.classList.toggle('hidden', !hasChildren)
+    })
   }
 
   static styles = ReBase.styles.concat([
     css`
       :host {
         display: inline-block;
-        padding: 1em;
-        border-width: 0.5rem;
+        --border-width: 0.5rem;
+        --neg-border: calc(-1 * var(--border-width));
+        border-width: var(--border-width);
+      }
+      slot {
+        display: block;
+      }
+      slot.hidden {
+        display: none;
+      }
+      slot:not([part=image]) {
+        padding: 1rem;
+      }
+      slot[part=image] {
+        position: relative; /* So image is above rough svg */
+        margin-top: var(--neg-border);
+        margin-left: var(--neg-border);
+        margin-right: var(--neg-border);
+      }
+      ::slotted(img) {
+        display: block;
+        width: 100%;
       }
     `
   ])
