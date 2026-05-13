acb0eeb1462394e65287c29f14f8d95c32607063
Roger Tawa <roger@tawacentral.net>
2026-05-13 17:21:06 -0400

    Add a part name to the rough svg element

--- src/re-element.ts
+++ src/re-element.ts
@@ -52,7 +52,7 @@ export class ReElement extends LitElement {
   }
 
   renderRoughSvg() {
-    return html`<svg xmlns="http://www.w3.org/2000/svg" id="rough"></svg>`
+    return html`<svg xmlns="http://www.w3.org/2000/svg" id="rough" part="rough"></svg>`
   }
 
   // This method is meant to be overidden by derived classes to handle
