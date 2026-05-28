705cb933f6f69fbad0e8f808b749f8d866e4ddbe
Roger Tawa <roger@tawacentral.net>
2026-05-28 00:04:36 -0400

    Fix re-divider look and sizing

--- src/re-divider.ts
+++ src/re-divider.ts
@@ -7,19 +7,23 @@ import { ReElement } from './re-element.js'
 export class DividerElement extends ReElement {
   @property({ type: Boolean, reflect: true }) vertical = false
 
-  static styles = [css`
-    :host {
-      display: block;
-      color: var(--color, inherit);
-      --thickness: 4px;
-      height: var(--thickness);
-    }
-    :host([vertical]) {
-      display: inline-block;
-      width: var(--thickness);
-      height: 100%;
-    }
-  `]
+  static styles = [
+    ...super.styles,
+    css`
+      :host {
+        display: block;
+        color: var(--color, inherit);
+        --thickness: 4px;
+        width: 100%;
+        height: var(--thickness);
+      }
+      :host([vertical]) {
+        display: inline-block;
+        width: var(--thickness);
+        height: 100%;
+      }
+    `
+  ]
 
   override render() {
     return super.renderRoughSvg()
@@ -29,16 +33,21 @@ export class DividerElement extends ReElement {
       width: number,
       height: number,
       cstyles: CSSStyleDeclaration): SVGElement[] {
+    console.log(`re-divider w=${width} h=${height}`)
     const thickness = Number.parseFloat(cstyles.getPropertyValue('--thickness'))
+
+    const options = Object.assign({
+    }, this.options)
 
     // The default randomness is 2 and stroke width is 1px.  This means lines
     // are up to 5px in "thickness".  Draw a new line for each 5px, but overlap
     // them a little.
     const lines = []
     for (let i = 2; i < thickness; i += 4) {
+      options.seed = (options.seed ?? 0) + 1
       lines.push(this.vertical
-          ? this.rough.line(i, 0, i, height)
-          : this.rough.line(0, i, width, i))
+          ? this.rough.line(i, 0, i, height, options)
+          : this.rough.line(0, i, width, i, options))
     }
     return lines
   }
