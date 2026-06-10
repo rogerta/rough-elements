7da91ad0c8697cbe5a41a40549434c9a738ede25
Roger Tawa <roger@tawacentral.net>
2026-06-10 13:21:57 -0400

    Makehover highlighting of re-menu-item consistent with the rest of the elements

--- src/re-menu-item.ts
+++ src/re-menu-item.ts
@@ -1,12 +1,18 @@
-import { css, html, LitElement, type PropertyValues } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { css, html, LitElement } from 'lit'
+import { customElement } from 'lit/decorators.js'
 
 import './re-icon.js'
 
 /**
  * An element that is meant to be used inside an <re-menu> element.  An
- * optional icon can be placed at the start.  An optional suffix can be used,
- * which is often used to show a keyboard shortcut.  If a submenu is specified
+ * optional icon can be placed at the start.  An optional suffix is often used
+ * to show a keyboard shortcut.
+ *
+ * The "body" part should be specified as an HTML element and not just a
+ * text node so that proper hover highlighting can be applied.  If the body
+ * is meant to be just text, it should be wrapped in a <span> element.
+ *
+ * If a submenu is specified
  * the suffix instead will show an icon to
  */
 @customElement('re-menu-item')
@@ -24,18 +30,21 @@ export class MenuItemElement extends LitElement {
       slot[name=suffix]::slotted(*), {
         flex: 0 0;
       }
-      slot[part=body], slot[part=body]::slotted(*) {
+      slot[part=body]::slotted(*) {
         display: inline-block;
         flex: 1 1;
         margin-left: 0.25rem;
         margin-right: 1rem;
       }
+
       @media (hover: hover) {
-        :host(:not([disabled]):hover) {
-          background: rgb(0 0 0 / 0.05);
+        :host(:hover:not([disabled])) ::slotted(*) {
+          text-shadow: 0 0 3px var(--button-text-shadow-color);
+          filter: drop-shadow(0px 0px 4px rgb(from var(--re-primary-color) R G B / 0.8));
         }
-        :host(:not([disabled]):hover:active) {
-          background: rgb(0 0 0 / 0.05);
+        :host(:hover:active:not([disabled])) ::slotted(*) {
+          text-shadow: 0 0 3px var(--button-text-shadow-color);
+          filter: drop-shadow(0px 0px 4px rgb(from var(--re-primary-color) R G B / 0.8));
         }
       }
     `
