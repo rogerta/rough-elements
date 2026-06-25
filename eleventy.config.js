c86ca74046bbaad392ca8398ebac37cbbe3796bc
Roger Tawa <roger@tawacentral.net>
2026-06-25 16:36:58 -0400

    Work on home page

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,12 @@
 export default function(config) {
   config.setInputDirectory('./docs')
   config.setOutputDirectory('./dist/docs')
+
+  // Copy `img/` to output.  Note: the input directory prefix
+  // is stripped from the file name before being copied to
+  // the output.
+  config.addPassthroughCopy('./docs/img')
+
   config.setQuietMode(true)
   config.setServerOptions({
     port: 9520,
