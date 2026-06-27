587fe957135eaa8853a7a8884edd9a2700e6313e
Roger Tawa <roger@tawacentral.net>
2026-06-26 22:56:46 -0400

    Add more filters to extract methods/fields/properties/slots/parts

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,20 +15,39 @@ export default function(config) {
     port: 9520,
   })
 
-  config.addFilter('filterForMethods', function (members) {
-    return members.filter(m => {
+  config.addFilter('filterForMethods', function (clazz) {
+    return clazz.members?.filter(m => {
       return m.kind === 'method' && m.privacy !== 'private' &&
       m.privacy !== 'protected' && !m.inheritedFrom &&
       m.name !== 'handleEvent'
-    })
+    }) ?? []
+  })
+
+  config.addFilter('filterForParts', function (clazz) {
+    return clazz.cssParts
+  })
+
+  config.addFilter('filterForCSSProperties', function (clazz) {
+    return clazz.cssProperties?.filter(p => !p.inheritedFrom) ?? []
+  })
+
+  config.addFilter('filterForProperties', function (clazz) {
+    // Don't bother documenting private and protected fields. Also, don't
+    // bother documenting the 'styles' field since it's Lit's internal CSS
+    // styling of the element.
+   return clazz.members?.filter(m => {
+      return m.kind === 'field' && m.privacy !== 'private' &&
+      m.privacy !== 'protected' && !m.inheritedFrom &&
+      m.name !== 'styles'
+    }) ?? []
   })
 
   config.addFilter('mapParam', function (arr) {
-    return arr ? arr.map(e => {
+    return arr?.map(e => {
       const name = e.name ?? ''
       const type = e.type?.text ?? ''
       return type ? `${name}: ${type}` : name
-    }): []
+    }) ?? []
   })
 
   config.addFilter('marked', function (str) {
